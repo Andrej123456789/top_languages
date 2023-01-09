083738f801643f22f0ef8e680e91576445dd24e0
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-01-09 21:45:11 +0100

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Description.vue'
 </script>
 
 <template>
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Top languages chart per repo!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -39,11 +39,11 @@ nav {
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+  color: hsla(160, 100%, 37%, 1);
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  background-color: rgb(22, 46, 22);
 }
 
 nav a {
--- src/components/Description.vue
+++ src/components/Description.vue
@@ -6,11 +6,9 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 style="color: hsla(160, 100%, 37%, 1)">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      Chart which shows which language is main language on all public repos (no forks)
     </h3>
   </div>
 </template>
